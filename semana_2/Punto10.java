11b04e2990d4ba7e97843243d340dd2f30521e2e
Daniel A <48231347+xDanix-a@users.noreply.github.com>
2026-09-12 01:12:23 +0000

    .

--- semana_2/Punto10.java
+++ semana_2/Punto10.java
@@ -1,5 +1,32 @@
 package semana_2;
 
+import java.util.Arrays;
+
 public class Punto10 {
-    
+    public static void main(String[] args) {
+        // Creación de los arreglos
+        int[] a = { 8, 5, 3, 10, 2, 8, 1 };
+        int[] b = {};
+        int tam = a.length; // tamaño del arreglo
+
+        if (a.length % 2 == 0) { //Cuando el arreglo es par
+            b = new int[tam / 2]; // Creación del arreglo d con la mitad del tamaño del arreglo original
+            for (int i = 0; i < tam / 2; i++) {
+                b[i] = a[i] + a[tam - 1 - i];
+            }
+        } else { //Cuando el arreglo es impar
+            b = new int[tam / 2 + 1]; // Creación del arreglo d con la mitad del tamaño del arreglo original + 1
+            for (int i = 0; i < a.length / 2 + 1; i++) {
+                if(i == (tam / 2)){
+                    b[i] = a[i];
+                }else{
+                    b[i] = a[i] + a[tam - 1 - i];
+                } 
+            }
+        }
+
+        // imprimir el arreglo
+        System.out.println(Arrays.toString(b));
+
+    }
 }
